4adb8ae0eb33ddbeeb0ae5cfcf5b3cad7d98a1e4
leuzery <valentyn.hvozdenko@htlstp.at>
2025-08-12 12:09:20 +0200

    config refactor

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,8 @@ interface Config {
     JWT_SECRET: string,
     MONGO_URI: string,
     MAX_FILE_SIZE: number,
-    MAX_USER_STORAGE: number
+    MAX_USER_STORAGE: number,
+    PUBLIC_KEY_PATH: string
 }
 
 export const config: Config = {
@@ -15,5 +16,6 @@ export const config: Config = {
     JWT_SECRET: process.env.JWT_SECRET_ADMIN || "secret",
     MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/htlgram",
     MAX_FILE_SIZE: Number(process.env.MAX_FILE_SIZE) || 50,
-    MAX_USER_STORAGE: Number(process.env.MAX_USER_STORAGE) || 50
+    MAX_USER_STORAGE: Number(process.env.MAX_USER_STORAGE) || 50,
+    PUBLIC_KEY_PATH: process.env.PRIVATE_KEY_PATH || 'jwtRS256.key.pub',
 }
