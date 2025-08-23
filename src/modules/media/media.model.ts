687bd4c00c625c910a801a3f37e5d30e48a88edd
leuzery <valentyn.hvozdenko@htlstp.at>
2025-08-23 17:27:28 +0200

    add spaceId for payload

--- src/modules/media/media.model.ts
+++ src/modules/media/media.model.ts
@@ -5,6 +5,11 @@ import { ErrorWithStatus } from "../../common/middleware/errorHandlerMiddleware"
 
 const PayloadSchema = new Schema<PayloadI>(
   {
+    spaceId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Space',
+      required: true
+    },
     communicationId: {
       type: Schema.Types.ObjectId,
       ref: 'Communication',
